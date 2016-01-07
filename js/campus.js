ea68053fbfadb4463a61df680ec8ae6d4a0867cd
Vitor Torreao <vitor.albuquerque@oncase.com.br>
2016-01-07 11:51:59 -0300

    First commit \o/

---
+++ js/campus.js
@@ -0,0 +1,39 @@
+var app = angular.module("campusApp", []);
+
+app.controller("courseList", function($scope) {
+	$scope.courses = [
+		{
+			name : "Dashboards com Pentaho",
+			details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit." + 
+				"Nunc eu lorem quis lacus cursus lacinia. Nunc fringilla luctus " +
+				"volutpat. Nunc dignissim eros ac urna dapibus viverra at id diam."+
+				" Vivamus tincidunt neque a orci blandit posuere. Integer feugiat "+
+				"vel velit nec condimentum. Aliquam at hendrerit ex, nec tristique"+
+				" ligula. Phasellus.",
+			image : "https://avatars1.githubusercontent.com/u/1022787?v=3&s=200",
+			tags : ['dashboards', 'pentaho'],
+			classes : [
+				{
+
+				}
+			],
+			workload : 20 //in hours
+		},
+		{
+			name : "Pentaho Analytics Bootcamp",
+			details : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "+
+				"In vestibulum risus eu lorem aliquam, in sollicitudin magna lobortis"+
+				". Aenean tincidunt feugiat orci, ut ultrices dolor iaculis scelerisque."+
+				" Maecenas mauris enim, bibendum in tincidunt eu, faucibus sit amet"+
+				" neque. Maecenas elementum.",
+			image : "http://statistiche.unisi.it:8080/pentaho-style/favicon.ico",
+			tags : ['bootcamp', 'analytics', 'pentaho'],
+			classes : [
+				{
+
+				}
+			],
+			workload : 40 //in hours
+		}
+	];
+});
