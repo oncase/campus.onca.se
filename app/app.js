3c8798ede61839f2d018176df25595d54ee2362f
Marcello Pontes <balaum@gmail.com>
2016-01-11 20:46:51 +0100

    Added theming + fixed share link input width for -xs devices

--- app/app.js
+++ app/app.js
@@ -18,11 +18,39 @@ define(
 				    console.log("Mudou!");
 				})
 		})
-		.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+		.config(function($mdThemingProvider){
 
-			// $locationProvider
-			//   .html5Mode(true)
-			// 	.hashPrefix('!');
+
+
+			      $mdThemingProvider.definePalette('oncasePalette', {
+			        '50': 'e5e9ef',
+			        '100': 'bfc8d8',
+			        '200': '95a4bf',
+			        '300': '6f84a8',
+			        '400': '4e6693',
+			        '500': '2b4a7f',
+			        '600': '284576',
+			        '700': '203c6c',
+			        '800': '1b3462',
+			        '900': '0e2854',
+			        'A100': '82a5ea',
+			        'A200': '3a7bff',
+			        'A400': '1f68ff',
+			        'A700': '135ffe',
+			        'contrastDefaultColor': 'light',
+			        'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100']
+			      });
+
+
+			      $mdThemingProvider
+
+			        .theme('default')
+			        .primaryPalette('oncasePalette')
+			        .accentPalette('deep-orange');
+
+
+		})
+		.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 
 			$urlRouterProvider.otherwise("/courses");
 
